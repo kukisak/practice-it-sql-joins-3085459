f843b341a6cd60aa25effb6eaa8f859e96cede2d
kukisak <petr.kukral@centrum.cz>
2023-05-10 08:02:16 +0000

    one-to-one relationship

---
+++ chapter-2.3.sql
@@ -0,0 +1,21 @@
+-- SQLite
+CREATE view vwCUSTOMER_ADDRESS
+as SELECT
+CustomerKey,
+AddressLine1,
+AddressLine2,
+GeographyKey
+FROM
+DimCustomer;
+
+CREATE view vwCUSTOMER_BIRTHDATE
+AS SELECT
+CustomerKey,
+Title,
+FirstName,
+MiddleName,
+LastName,
+NameStyle,
+BirthDate
+FROM 
+DimCustomer
