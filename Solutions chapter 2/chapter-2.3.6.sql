f718d56eb42e1a74bd98cb60fc603560c1312315
kukisak <petr.kukral@centrum.cz>
2023-05-10 08:32:32 +0000

    Self join

---
+++ Solutions chapter 2/chapter-2.3.6.sql
@@ -0,0 +1,10 @@
+-- SQLite
+-- Self join
+SELECT A.EnglishProductName,
+B.EnglishProductName,
+A.ProductSubcategoryKey
+FROM DimProduct A
+INNER JOIN DimProduct B
+ON A.ProductKey <> B.ProductKey
+AND A.ProductSubcategoryKey = B.ProductSubcategoryKey
+ORDER BY A.ProductSubcategoryKey
